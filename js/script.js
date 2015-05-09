c420ad0b143b486d96e3bb7af1b1c5e73976c843
Outsider <outsideris@gmail.com>
2015-05-09 18:14:09 +0900

    generate meta tags for open graph

---
+++ js/script.js
@@ -0,0 +1,47 @@
+;(function() {
+  'use strict';
+
+  var body = document.body;
+  var meta = document.querySelector('#meta');
+
+  // inputs
+  var name = document.querySelector('#name'),
+      title = document.querySelector('#title'),
+      desc = document.querySelector('#desc'),
+      url = document.querySelector('#url'),
+      image = document.querySelector('#image');
+
+
+  body.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var tags = generateMeta();
+
+    meta.innerHTML = tags;
+  });
+
+  function template(t) {
+    return function(o) {
+      return t.replace(/\{([^{}]*)\}/g, function(a, b) {
+        var r = o[b];
+        return typeof r === 'string' || typeof r === 'number' ? r : a;
+      });
+    };
+  }
+
+  var ogName = template('<meta property="og:site_name" content="{name}">'),
+      ogTitle = template('<meta property="og:title" content="{title}">'),
+      ogDesc = template('<meta property="og:description" content="{desc}">'),
+      ogUrl = template('<meta property="og:url" content="{url}">'),
+      ogImage = template('<meta property="og:image" content="{image}">');
+
+  function generateMeta() {
+    var tags = [];
+    tags.push(ogName({name: name.value}));
+    tags.push(ogTitle({title: title.value}));
+    tags.push(ogDesc({desc: desc.value}));
+    tags.push(ogUrl({url: url.value}));
+    tags.push(ogImage({image: image.value}));
+    return tags.join('\n');
+  }
+
+})();
